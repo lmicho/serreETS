91d02ee59989ed8758686df29da08bb33a2b0c59
chino1921 <laurianemichaud@hotmail.com>
2015-10-01 15:20:31 -0400

    form bug fixed

--- index.php
+++ index.php
@@ -10,9 +10,12 @@
         <!-- ==== HEADERWRAP ==== -->
         <div id="headerwrap" name="home" id="home">
             <header class="clearfix">
+                <img src="img/logo_small.png" alt="serreÉTS" width="150" height="196" />
                 <h1>Le génie du développement durable</h1>
                 <p>Lorem ipsum dolor sit amet, cu menandri molestiae voluptaria eam,<br>
-                    invidunt reprehendunt nec ei. Sonet regione consulatu vel id.</p>
+                    invidunt reprehendunt nec ei. Sonet regione consulatu vel id.
+                </p>
+
                 <a href="#about" class="smoothScroll btn btn-lg">Notre mission</a> 
             </header>
         </div>
@@ -176,81 +179,8 @@
                     </div>
                     <div class="col-lg-8 col-lg-offset-2 centered">
 
-                        <form id="enlisting" method="post" class="form" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                            <div class="row">
-                                <div class="col-xs-6 col-md-6 form-group">
-                                    <input class="form-control" id="firstName" name="firstName" placeholder="Prénom" type="text" required />
-                                </div>
-                                <div class="col-xs-6 col-md-6 form-group">
-                                    <input class="form-control" id="lastName" name="lastName" placeholder="Nom" type="text" required />
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-6 col-md-6 form-group">
-                                    <select class="form-control" id="program" name="program">
-                                        <option>Programme d'études</option>
-                                        <option>Génie construction</option>
-                                        <option>Génie mécanique</option>
-                                        <option>Génie électrique</option>
-                                        <option>Génie production automatisée</option>
-                                        <option>Génie gestion des opérations</option>
-                                        <option>Génie logiciel</option>
-                                        <option>Génie technologie de l'information</option>
-                                    </select>
-                                </div>
-                                <div class="col-xs-6 col-md-6 form-group">
-                                    <input class="form-control" id="email" name="email" placeholder="Courriel" type="email" required />
-                                </div>
-                            </div>
+                        <?php include "includes/enlisting_form.php"; ?>
 
-                            <textarea class="form-control" id="message" name="message" placeholder="Message" rows="5"></textarea>
-                            <div class="row">
-                                <div class="col-xs-12 col-md-12">
-                                    <button class="btn btn btn-lg" type="submit" name="enlisting-submit">Envoyer</button>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <?php
-                                //if "email" variable is filled out, send email
-                                if (isset($_POST['enlisting-submit'])) {
-
-                                  //Email information
-                                  $admin_email = "lauriane.b.michaud@gmail.com";
-
-                                  //subject
-                                  $subject = 'Recrutement SerreETS';
-
-                                  // message
-                                  $firstName = @trim(stripslashes($_POST['firstName']));
-                                  $lastName = @trim(stripslashes($_POST['lastName']));
-                                  $program = @trim(stripslashes($_POST['program']));
-                                  $email = @trim(stripslashes($_POST['email']));
-                                  $message2 = @trim(stripslashes($_POST['message']));
-
-                                  $message = '
-                                  <html>
-                                  <body>
-                                    <p>Une nouvelle personne est intéressée à faire partie du club.</p><br />
-                                    <p>Nom : ' . $firstName . ' ' . $lastName . '</p>
-                                    <p>Programme d\'études : ' . $program . '</p>
-                                    <p>Courriel : ' . $email . '</p>
-                                    <p>Message : ' . $message2 . '</p>
-                                  </body>
-                                  </html>';
-
-                                  // To send HTML mail, the Content-type header must be set
-                                  $headers = 'MIME-Version: 1.0' . "\r\n";
-                                  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-                                  //send email
-                                  mail($admin_email, $subject, $message, $headers);
-
-                                  //Email response
-                                  echo '<p>Votre message a bien été envoyé. Merci.</p>';
-                                }
-                                ?>
-                            </div>
-                        </form>
                     </div>
                 </div>
                 <!-- row --> 
@@ -277,70 +207,9 @@
                 <div class="row">
                     <div class="col-lg-8 col-lg-offset-2 centered">
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
-                        <form id="contact" method="post" class="form" role="form" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-                            <div class="row">
-                                <div class="col-xs-6 col-md-6 form-group">
-                                    <input class="form-control" id="name" name="name" placeholder="Nom" type="text" required />
-                                </div>
-                                <div class="col-xs-6 col-md-6 form-group">
-                                    <input class="form-control" id="email" name="email2" placeholder="Courriel" type="email" required />
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-xs-12 col-md-12 form-group">
-                                    <select class="form-control" id="sel1" name="subject">
-                                        <option>Sujet</option>
-                                        <option>Question</option>
-                                        <option>Partenariat</option>
-                                        <option>Financement</option>
-                                        <option>Recrutement</option>
-                                    </select>
-                                </div>
-
-                            </div>
-                            <textarea class="form-control" id="message" name="message2" placeholder="Message" rows="5"></textarea>
-                            <div class="row">
-                                <div class="col-xs-12 col-md-12">
-                                    <button class="btn btn btn-lg" type="submit" name="contactus-submit">Envoyer</button>
-                                </div>
-                            </div>
-                            <div class="row">
-
-                                <?php
-                                //if "email" variable is filled out, send email
-                                if (isset($_POST['contactus-submit'])) {
-
-                                  //Email information
-                                  $admin_email = "lauriane.b.michaud@gmail.com";
-
-                                  // message
-                                  $name = @trim(stripslashes($_POST['name']));
-                                  $email = @trim(stripslashes($_POST['email2']));
-                                  $subject = @trim(stripslashes($_POST['subject']));
-                                  $message2 = @trim(stripslashes($_POST['message2']));
-
-                                  $message = '
-                                    <html>
-                                    <body>
-                                      <p>' . $message2 . '</p>
-                                      <p>De : ' . $name . '</p>
-                                      <p>Courriel : ' . $email . '</p>
-                                    </body>
-                                    </html>';
-
-                                  // To send HTML mail, the Content-type header must be set
-                                  $headers = 'MIME-Version: 1.0' . "\r\n";
-                                  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-
-                                  //send email
-                                  mail($admin_email, $subject, $message, $headers);
-
-                                  //Email response
-                                  echo '<br /><p>Votre message a bien été envoyé. Merci.</p>';
-                                }
-                                ?>
-                            </div>
-                        </form>
+                        
+                        <?php include "includes/contact_form.php"; ?>
+                        
                     </div>
                 </div>
                 <!-- form --> 
